5b4e78927294979efa377e64b019da03cd360abd
David Stauffer <dstauffman@yahoo.com>
2015-12-16 16:59:52 -0800

    Update enum example documentation and switch order of values.

--- enum/GENDER_.m
+++ enum/GENDER_.m
@@ -1,27 +1,29 @@
 classdef(Enumeration) GENDER_ < int32
-    
+
 % GENDER_ is a enumerator class definition for the possible genders.
 %
 % Input:
 %     None
 %
 % Output:
-%     GENDER_        : (enum) possible values
+%     GENDER_ ...... : (enum) possible values
 %         null   = 0 : not a valid setting, used to preallocate
-%         male   = 1 : uncircumcised male
-%         female = 2 : female
+%         female = 1 : female
+%         male   = 2 : male
 %
 % Prototype:
 %     GENDER_.female         % returns female as an enumeratod GENDER_ type
-%     double(GENDER_.female) % returns 2, which is the enumerated value of GENDER_.female
+%     double(GENDER_.female) % returns 1, which is the enumerated value of GENDER_.female
 %     char(GENDER_.female)   % returns 'female' as a character string
+%     class(GENDER_.female)  % returns 'GENDER_' as a character string
 %
 % Change Log:
 %     1.  Written by David Stauffer in June 2013.
+%     2.  Added to DStauffman MATLAB library in December 2015.
 
     enumeration
         null(0)
-        male(1)
-        female(2)
+        female(1)
+        male(2)
     end
 end
