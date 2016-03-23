0bfb5fd08a9f6cb362a05c4b3851dd9c8b12a9d5
David Stauffer <dstauffman@yahoo.com>
2016-03-22 17:08:45 -0700

    Example subclass (even though this class is sealed and won't work.)

---
+++ classes/MyFigure.m
@@ -0,0 +1,16 @@
+classdef MyFigure < matlab.ui.Figure
+    
+    % Custom Figure with sorting based on Number
+    
+    methods
+        function obj = MyFigure(varargin)
+            obj@matlab.ui.Figure(varargin)
+        end
+        
+        function [obj, idx] = sort(obj, varargin)
+            % Sort Figure array with respect to 'Number' property
+            [~,idx] = sort([obj.Number], varargin{:});
+            obj = obj(idx);
+        end
+    end
+end
