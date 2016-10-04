a7853adb0bcede493d789fcca5a58fd9c3d44db8
David Stauffer <dstauffman@yahoo.com>
2016-10-04 15:38:56 -0700

    Make setup_dir optionally not wipe the contents of an existing directory.

--- utils/setup_dir.m
+++ utils/setup_dir.m
@@ -1,4 +1,4 @@
-function [] = setup_dir(directory)
+function [] = setup_dir(directory, wipe)
 
 % SETUP_DIR  clears contents or instantiates directory for writing output files.
 %
@@ -32,6 +32,17 @@ function [] = setup_dir(directory)
 % Change Log:
 %     1.  Added to DStauffman's library from GARSE in Sept 2013.
 %     2.  Modified to be fully recursive by David Stauffer in Sept 2013.
+%     3.  Modified by David C. Stauffer to optionally not wipe existing directories in October 2016.
+
+% check for optional inputs
+switch nargin
+    case 1
+        wipe = true;
+    case 2
+        % nop
+    otherwise
+        error('dstauffman:UnexpectedNargin', 'Unexpected number of inputs: "%s"', nargin);
+end
 
 % add filesep to end of path if necessary
 if ~strcmp(directory(end),filesep)
@@ -40,21 +51,25 @@ end
 
 % empty files within directory if it already exists
 if exist(directory,'dir')
-    contents = dir(directory);
-    names    = {contents(3:end).name};
-    isdir    = [contents(3:end).isdir];
-    for i = 1:length(names)
-        if isdir(i)
-            % call recursively for subfolders
-            setup_dir([directory,names{i}]);
-        else
-            % delete files
-            delete([directory,names{i}]);
+    if wipe
+        contents = dir(directory);
+        names    = {contents(3:end).name};
+        isdir    = [contents(3:end).isdir];
+        for i = 1:length(names)
+            if isdir(i)
+                % call recursively for subfolders
+                setup_dir([directory,names{i}]);
+            else
+                % delete files
+                delete([directory,names{i}]);
+            end
         end
-    end
-    % display that something was removed
-    if ~isempty(names)
-        disp(['Files/Sub-folders were removed from "',directory,'"']);
+        % display that something was removed
+        if ~isempty(names)
+            disp(['Files/Sub-folders were removed from "',directory,'"']);
+        end
+    else
+        disp(['Directory "',directory,'" already exists, and was not wiped.'])
     end
 else
     % create directory if it does not exist
