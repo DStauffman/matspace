e68953a313114995cec438710f0c893a19327c10
David Stauffer <dstauffman@yahoo.com>
2016-11-17 15:06:54 -0800

    Initial Release.

---
+++ tests/test_maxmax.m
@@ -0,0 +1,92 @@
+classdef test_maxmax < matlab.unittest.TestCase %#ok<*PROP>
+    
+    % Tests the maxmax function with the following cases:
+    % (1) 1 Argument
+    %    (a) 10 x 1
+    %    (b) 1 x 9
+    %    (c) 3 x 4
+    %    (d) 3 x 4 x 6
+    %    (e) 3 x 4 x 2 x 5
+    % (2) dim specified
+    %    (a) 10 x 1, 1
+    %    (b) 10 x 1, 2
+    %    (c) 3 x 4, 1
+    %    (d) 3 x 4, 2
+    %    (e) 3 x 1 x 6, [1 2]
+    %    (f) 3 x 1 x 6, [2 3]
+    %    (g) 3 x 4 x 2 x 5, (2:4)
+    
+    methods (Test)
+        function test_1a(self)
+            % Case 1a
+            x = randn(10,1);
+            self.verifyEqual(maxmax(x), max(x))
+        end
+        
+        function test_1b(self)
+            % Case 1b
+            x = randn(1,9);
+            self.verifyEqual(maxmax(x), max(x))
+        end
+        
+        function test_1c(self)
+            % Case 1c
+            x = randn(3,4);
+            self.verifyEqual(maxmax(x), max(max(x)))
+        end
+        
+        function test_1d(self)
+            % Case 1d
+            x = randn(3,4,6);
+            self.verifyEqual(maxmax(x), max(max(max(x))))
+        end
+        
+        function test_1e(self)
+            % Case 1e
+            x = randn(3,4,2,5);
+            self.verifyEqual(maxmax(x), max(max(max(max(x)))))
+        end
+        
+        function test_2a(self)
+            % Case 2a
+            x = randn(10,1);
+            self.verifyEqual(maxmax(x, 1), max(x))
+        end
+        
+        function test_2b(self)
+            % Case 2b
+            x = randn(10,1);
+            self.verifyEqual(maxmax(x, 2), x)
+        end
+        
+        function test_2c(self)
+            % Case 2c
+            x = randn(3,4);
+            self.verifyEqual(maxmax(x, 1), max(x, [], 1))
+        end
+        
+        function test_2d(self)
+            % Case 2d
+            x = randn(3,4);
+            self.verifyEqual(maxmax(x, 2), max(x, [] ,2))
+        end
+        
+        function test_2e(self)
+            % Case 2e
+            x = randn(3,1,6);
+            self.verifyEqual(maxmax(x, [1 2]), max(max(x,[],1),[],2))
+        end
+        
+        function test_2f(self)
+            % Case 2f
+            x = randn(3,1,6);
+            self.verifyEqual(maxmax(x, [2 3]), max(max(x,[],2),[],3))
+        end
+        
+        function test_2g(self)
+            % Case 2g
+            x = randn(3,4,2,5);
+            self.verifyEqual(maxmax(x, 2:4), max(max(max(x,[],2),[],3),[],4))
+        end
+    end
+end
---
+++ utils/minmin.m
@@ -0,0 +1,58 @@
+function [result] = minmin(x, dim)
+
+% MINMIN  Minimum over multiple dimensions.
+%
+% Summary:
+%     minmin(x) will return a scalar that is the minimum over all the elements
+%     of x, regardless of dimensionality.
+%
+%     minmin(x, dim) operates along the dimensions specified in dim.
+%
+%     Example: if X is 3x4x2, then
+%         minmin(X) is the minimum over all three dimensions, but
+%         minmin(X, [1 3]) will return a 1x4 vector.
+%
+% Input:
+%     x      : matrix to be minimized
+%     dim    : |opt| dimensions of x upon which to operate
+%
+% Output:
+%     result : scalar minimum of x, unless optional dim was provided, in
+%              which case the dimensions are determined by the dimensions not
+%              minimized over.
+%
+% See also:
+%     min, maxmax
+%
+% Change Log:
+%     1.  Written by Keith Rogers for SSC Toolbox, last updated Feb 2012.
+%     2.  Incorporated by David C. Stauffer into DStauffman library in Nov 2016.
+
+% Not EML Compliant due to for loop (Dimension argument must be constant)
+
+% get sizes of all dimensions of x
+d = size(x);
+
+% check for optional inputs
+switch nargin
+    case 1
+        % find all non-singelton dimensions
+        dim = find(d > 1);
+        if length(d) < 2
+            % Simple vector input case
+            result = min(x);
+            return
+        end
+    case 2
+        % nop
+    otherwise
+        error('dstauffman:UnexpectedNargin', 'Unexpected number of inputs: "%s"', nargin);
+end
+
+% preallocate to input, for case where no dimensions are specified
+result = x;
+
+% loop through and shrink to min of each dimension in sequence
+for i = dim
+    result = min(result, [], i);
+end
