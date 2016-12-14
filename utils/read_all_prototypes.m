7fde968dcaebcee3bcc15e90ba1666607577fd54
David Stauffer <dstauffman@yahoo.com>
2016-12-14 13:18:27 -0800

    Change order of inputs and improve error statements.

--- utils/read_all_prototypes.m
+++ utils/read_all_prototypes.m
@@ -1,4 +1,4 @@
-function [counter] = read_all_prototypes(folder,fid,counter,recursive)
+function [counter] = read_all_prototypes(folder,recursive,fid,counter)
 
 % READ_ALL_PROTOTYPES  reads all the prototypes from the specified folder
 %
@@ -7,9 +7,9 @@ function [counter] = read_all_prototypes(folder,fid,counter,recursive)
 %
 % Input:
 %     folder    : (row) string specifying the folder name [char]
+%     recursive : (scalar) true/false flag for whether processing recursively [bool]
 %     fid       : (scalar) file identifier [num]
 %     counter   : (scalar) function counter to start from [num]
-%     recursive : (scalar) true/false flag for whether processing recursively [bool]
 %
 % Output:
 %     counter   : final counter of prototypes found [num]
@@ -17,10 +17,9 @@ function [counter] = read_all_prototypes(folder,fid,counter,recursive)
 %
 % Prototype:
 %     folder    = get_root_dir();
-%     fid       = [];
 %     recursive = true;
 %     disp('This prototype rocks (and can otherwise get you in an infinite loop).');
-%     % read_all_prototypes(folder,fid,recursive);
+%     % read_all_prototypes(folder,recursive);
 %
 % Change Log:
 %     1.  Written by David C. Stauffer circa 2011.
@@ -35,22 +34,22 @@ ignore_classes    = true;
 switch nargin
     case 0
         folder    = pwd;
+        recursive = true;
         fid       = [];
         counter   = 0;
-        recursive = true;
     case 1
+        recursive = true;
         fid       = [];
         counter   = 0;
-        recursive = true;
     case 2
-        recursive = true;
+        fid       = [];
         counter   = 0;
     case 3
         counter   = 0;
     case 4
         % nop
     otherwise
-        error('Unexpected number of inputs.');
+        error('dstauffman:UnexpectedNargin', 'Unexpected number of inputs: "%s"', nargin);
 end
 
 % open file if not already specified
@@ -58,7 +57,7 @@ if isempty(fid)
     filename = fullfile(pwd,'all_prototypes.m');
     fid = fopen(filename,'wt');
     if fid == -1
-        error('Unable to open "%s" for writing.',filename);
+        error('dstauffman:AllPrototypesWriting','Unable to open "%s" for writing.',filename);
     end
     fprintf(fid,'%s\n','function [] = all_prototypes() %#ok<*DEFNU>');
     fprintf(fid,'%s\n\n','    main()');
@@ -75,7 +74,7 @@ for i = 3:length(list)
     if list(i).isdir
         % if processing recursively, then call this function on the subfolder, otherwise break out of this iteration
         if recursive && ~any(strcmp(list(i).name,folder_exclusions))
-            counter = read_all_prototypes(this_name,fid,counter,recursive);
+            counter = read_all_prototypes(this_name,recursive,fid,counter);
         else
             continue
         end
@@ -86,7 +85,7 @@ for i = 3:length(list)
             % read this m file
             temp_fid = fopen(this_name,'rt');
             if temp_fid == -1
-                error('Unable to open "%s" for reading.',this_name);
+                error('dstauffman:AllPrototypesReading','Unable to open "%s" for reading.',this_name);
             end
             % read file
             lines = textscan(temp_fid,'%s',inf,'Delimiter','\n');
@@ -102,7 +101,7 @@ for i = 3:length(list)
             ix2 = union(find_lines(lines,'% See Also:'), find_lines(lines,'% Change Log:'));
             % throw warning if prototype is not found
             if isempty(ix1) || isempty(ix2)
-                warning('utils:findPrototype','PROTOTYPE not found for <a href="matlab: web(''%s'',''-browser'');">%s</a>',this_name,this_name);
+                warning('utils:findPrototype','Prototype not found for <a href="matlab: web(''%s'',''-browser'');">%s</a>',this_name,this_name);
                 continue
             end
             % increment counter
