ce48e26635bfe7ea09962eb95180539b68f3d296
David Stauffer <dstauffman@yahoo.com>
2018-01-16 16:06:05 -0800

    Allow lines to be excluded from legend.

--- plotting/plot_rms_lines.m
+++ plotting/plot_rms_lines.m
@@ -1,4 +1,4 @@
-function plot_rms_lines(x,y)
+function plot_rms_lines(x,y,show_in_legend)
 
 % PLOT_RMS_LINES  plots a vertical line at the RMS start and stop times.
 %
@@ -10,6 +10,7 @@ function plot_rms_lines(x,y)
 % Input:
 %     x : (1x2) vector of xmin and xmax values at which to draw the lines [num]
 %     y : (1x2) vector of ymin and ymax values at which to extend the lines vertically [num]
+%     show_in_legend : (scalar), optional, show the lines when a legend is turned on [bool]
 %
 % Output:
 %     (NONE)
@@ -24,18 +25,35 @@ function plot_rms_lines(x,y)
 %
 % Change Log:
 %     1.  Added to DStauffman's MATLAB libary from GARSE in Sept 2013.
+%     2.  Updated by David C. Stauffer in January 2018 to allow to be excluded from legends.
+
+% get optional inputs
+switch nargin
+    case 2
+        show_in_legend = true;
+    case 3
+        % nop
+    otherwise
+        error('dstauffman:UnexpectedNargin','Unexpected number of inputs: "%i"',nargin);
+end
 
 % initial figure hold state
 hold_state = ishold;
 hold on;
 
-% Bug in Matlab when using MarkerEdgeColor and the plotbrowser (for R2014A (8.3) to R2015B (9.6), fixed in R2016A (9.0)):
+% Bug in Matlab when using MarkerEdgeColor and the plotbrowser (for R2014A (8.3) to R2015B (8.6), fixed in R2016A (9.0)):
 if ~verLessThan('matlab', '9.0') || verLessThan('matlab','8.3')
-    plot([x(1) x(1)],y,'LineStyle','--','Color',[   1 0.75 0],'Marker','+','MarkerEdgeColor','m','MarkerSize',10,'DisplayName','RMS Start Time');
-    plot([x(2) x(2)],y,'LineStyle','--','Color',[0.75 0.75 1],'Marker','+','MarkerEdgeColor','m','MarkerSize',10,'DisplayName','RMS Stop Time');
+    h1 = plot([x(1) x(1)],y,'LineStyle','--','Color',[   1 0.75 0],'Marker','+','MarkerEdgeColor','m','MarkerSize',10,'DisplayName','RMS Start Time');
+    h2 = plot([x(2) x(2)],y,'LineStyle','--','Color',[0.75 0.75 1],'Marker','+','MarkerEdgeColor','m','MarkerSize',10,'DisplayName','RMS Stop Time');
 else
-    plot([x(1) x(1)],y,'LineStyle','--','Color',[   1 0.75 0],'Marker','+','MarkerSize',10,'DisplayName','RMS Start Time');
-    plot([x(2) x(2)],y,'LineStyle','--','Color',[0.75 0.75 1],'Marker','+','MarkerSize',10,'DisplayName','RMS Stop Time');
+    h1 = plot([x(1) x(1)],y,'LineStyle','--','Color',[   1 0.75 0],'Marker','+','MarkerSize',10,'DisplayName','RMS Start Time');
+    h2 = plot([x(2) x(2)],y,'LineStyle','--','Color',[0.75 0.75 1],'Marker','+','MarkerSize',10,'DisplayName','RMS Stop Time');
+end
+
+% exclude lines from legend
+if ~show_in_legend
+    set(get(get(h1,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+    set(get(get(h2,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
 end
 
 % reset hold state if it wasn't previously set
