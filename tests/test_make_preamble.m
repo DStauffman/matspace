1e5ed0abfde0ab128f09b9079e3ecb90ba573c1f
David Stauffer <dstauffman@yahoo.com>
2018-07-06 21:10:07 -0700

    Fix tests to pass with non-escaped slashes.

--- tests/test_make_preamble.m
+++ tests/test_make_preamble.m
@@ -23,33 +23,33 @@ classdef test_make_preamble < matlab.unittest.TestCase %#ok<*PROP>
     methods(Test)
         function test_nominal(self)
             out = make_preamble(self.caption, self.label, self.cols);
-            self.verifyTrue(any(strcmp(out,'    \\caption{This caption}%')));
-            self.verifyTrue(any(strcmp(out,'    \\label{tab:this_label}')));
-            self.verifyTrue(any(strcmp(out,'    \\begin{tabular}{lcc}')));
-            self.verifyTrue(any(strcmp(out,'    \\small')));
+            self.verifyTrue(any(strcmp(out,'    \caption{This caption}%')));
+            self.verifyTrue(any(strcmp(out,'    \label{tab:this_label}')));
+            self.verifyTrue(any(strcmp(out,'    \begin{tabular}{lcc}')));
+            self.verifyTrue(any(strcmp(out,'    \small')));
         end
 
         function test_size(self)
-            out = make_preamble(self.caption, self.label, self.cols, 'Size', '\\footnotesize');
-            self.verifyTrue(any(strcmp(out,'    \\footnotesize')));
-            self.verifyFalse(any(strcmp(out,'    \\small')));
+            out = make_preamble(self.caption, self.label, self.cols, 'Size', '\footnotesize');
+            self.verifyTrue(any(strcmp(out,'    \footnotesize')));
+            self.verifyFalse(any(strcmp(out,'    \small')));
         end
 
         function test_minipage(self)
             out = make_preamble(self.caption, self.label, self.cols, 'UseMini', true);
-            self.verifyTrue(any(strcmp(out,'    \\begin{minipage}{\\linewidth}')));
-            self.verifyTrue(any(strcmp(out,'        \\begin{tabular}{lcc}')));
+            self.verifyTrue(any(strcmp(out,'    \begin{minipage}{\linewidth}')));
+            self.verifyTrue(any(strcmp(out,'        \begin{tabular}{lcc}')));
         end
 
         function test_short_cap(self)
             out = make_preamble(self.caption, self.label, self.cols, 'ShortCap', 'Short cap');
-            self.verifyTrue(any(strcmp(out,'    \\caption[Short cap]{This caption}%')));
-            self.verifyFalse(any(strcmp(out,'    \\caption{This caption}%')));
+            self.verifyTrue(any(strcmp(out,'    \caption[Short cap]{This caption}%')));
+            self.verifyFalse(any(strcmp(out,'    \caption{This caption}%')));
         end
 
         function test_numbered_false1(self)
             out = make_preamble(self.caption, self.label, self.cols, 'Numbered', false);
-            self.verifyTrue(any(strcmp(out,'    \\caption*{This caption}%')));
+            self.verifyTrue(any(strcmp(out,'    \caption*{This caption}%')));
         end
 
         function test_numbered_false2(self)
