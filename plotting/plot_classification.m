78c1db94aa90c38f652e873436798e870ac4e560
David Stauffer <david.c.stauffer@lmco.com>
2020-04-13 18:04:17 -0700

    Fix sizing issues and let you re-mark a plot with a different option.

--- plotting/plot_classification.m
+++ plotting/plot_classification.m
@@ -18,14 +18,14 @@ function [] = plot_classification(fig_hand, classification, caveat, test, inside
 %
 % Prototype:
 %     f1 = figure;
-%     plot([0 1],[0 1],'.-b');
-%     plot_classification(f1,'U');
+%     plot([0 1], [0 1], '.-b');
+%     plot_classification(f1, 'U');
 %     f2 = figure;
-%     plot(0,0);
-%     plot_classification(f2,'S','//MADE UP CAVEAT',true, false);
+%     plot(0, 0);
+%     plot_classification(f2, 'S', '//MADE UP CAVEAT', true, false);
 %     f3 = figure;
-%     plot(0,0);
-%     plot_classification(f3,'C','', true, true);
+%     plot(0, 0);
+%     plot_classification(f3, 'C', '', true, true);
 %
 %     % clean up
 %     close([f1 f2 f3]);
@@ -38,6 +38,8 @@ function [] = plot_classification(fig_hand, classification, caveat, test, inside
 %     2.  Updated by David C. Stauffer in Sep 2011 to not be effected by panning and zooming.
 %     3.  Incorporated by David C. Stauffer into matspace library in Nov 2016.
 %     4.  Updated by David C. Stauffer in March 2020 to include optional caveats.
+%     5.  Updated by David C. Stauffer in April 2020 to remove old markers and handle variable sizes
+%         of text better.
 
 %% check for optional inputs
 switch nargin
@@ -78,17 +80,20 @@ for i = 1:length(fig_hand)
     else
         this_fig = fig_hand(i);
     end
+    % remove any pre-existing classification annotations
+    remove_old_classifications(this_fig);
     % get the position of the axis within the figure
-    ax  = get(this_fig,'CurrentAxes');
-    pos = get(ax,'Position');
+    ax  = get(this_fig, 'CurrentAxes');
+    pos = get(ax, 'Position');
     % plot warning before trying to draw the other box
     if test
         w = 0.6;
         h = 0.1;
-        annotation(this_fig,'textbox','Position',[pos(1)+pos(3)/2-w/2,pos(2)+pos(4)-h,w,h],...
-            'String','This plot classification is labeled for test purposes only',...
-            'HorizontalAlignment','Center','VerticalAlignment','Middle',...
-            'FontSize',8,'FontWeight','Bold','Color','r','EdgeColor','r','LineWidth',2);
+        annotation(this_fig, 'textbox' ,'Position', [pos(1)+pos(3)/2-w/2,pos(2)+pos(4)-h,w,h], ...
+            'String', 'This plot classification is labeled for test purposes only', ...
+            'HorizontalAlignment', 'Center', 'VerticalAlignment', 'Middle', 'FitBoxToText', 'on', ...
+            'FontSize', 8, 'FontWeight', 'Bold', 'Color', 'r', 'EdgeColor', 'r', 'LineWidth', 2, ...
+            'Tag', 'ClassificationTest');
     end
     % get classification colors and strings
     switch classification
@@ -110,12 +115,12 @@ for i = 1:length(fig_hand)
     end
     text_color = color;
     % determine size of box and append any optional caveat(s)
-    h = 0.1;
+    h = 0.05;
     if ~isempty(caveat)
         text_str = [text_str, caveat]; %#ok<AGROW>
-        w = 0.4; % TODO: could make depend on length of caveat
+        w = 0.6;
     else
-        w = 0.2;
+        w = 0.3;
     end
     % allow other color options for certain caveats
     if contains(caveat, '//FAKE COLOR')
@@ -124,13 +129,33 @@ for i = 1:length(fig_hand)
     end
     % add classification box
     if inside_axes
-        annotation(this_fig,'textbox','Position',[pos(1)+pos(3)-w,pos(2),w,h],'String',text_str,...
-            'HorizontalAlignment','Center','VerticalAlignment','Middle',...
-            'FontSize',8,'FontWeight','Bold','Color',text_color,'EdgeColor',color,'LineWidth',2);
+        annotation(this_fig, 'textbox', 'Position', [pos(1)+pos(3)-w,pos(2),w,h], 'String', text_str, ...
+            'HorizontalAlignment', 'Right', 'VerticalAlignment', 'Middle', 'FitBoxToText', 'on', ...
+            'FontSize', 8, 'FontWeight', 'Bold', 'Color', text_color, 'EdgeColor', color, ...
+            'LineWidth', 2, 'Tag', 'ClassificationText');
     else
-        annotation(this_fig,'textbox','Position',[1-w,0,w,h],'String',text_str,...
-            'HorizontalAlignment','Center','VerticalAlignment','Middle',...
-            'FontSize',8,'FontWeight','Bold','Color',text_color,'EdgeColor',color,'LineWidth',2);
-        annotation(this_fig,'rectangle','Position',[0,0,1,1],'Color','none','EdgeColor',color,'LineWidth',2);
+        annotation(this_fig, 'textbox', 'Position', [1-w,0,w,h], 'String', text_str,...
+            'HorizontalAlignment', 'Right', 'VerticalAlignment', 'Middle', 'FitBoxToText', 'on', ...
+            'FontSize', 8, 'FontWeight', 'Bold', 'Color', text_color, 'EdgeColor', color, ...
+            'LineWidth', 2, 'Tag', 'ClassificationText');
+        annotation(this_fig, 'rectangle', 'Position', [0,0,1,1], 'Color', 'none', 'EdgeColor', color, ...
+            'LineWidth',2, 'Tag', 'ClassificationBorder');
     end
+end
+
+
+function remove_old_classifications(this_fig)
+
+% REMOVE_OLD_CLASSIFICATIONS  removes any pre-existing classification markings so that they can
+%                             be replaced with new ones without clashing
+
+% Return the axes that contains the annotation objects
+ax = findall(this_fig, 'Tag', 'scribeOverlay');
+% Find the Children with type hggroup
+anons = get(ax, 'Children');
+% find the ones labeled as classifications
+ix = startsWith(arrayfun(@(x) x.Tag, anons, 'UniformOutput', false), 'Classification');
+% delete them
+if any(ix)
+    delete(anons(ix));
 end
