6d2ade2e876aec912446d33c7fc5efcc9813d0e6
David Stauffer <david.c.stauffer@lmco.com>
2020-06-03 17:42:43 -0700

    Finally add a working intersect2 function that I started all those years ago (circa Feb 2010).

--- +matspace/+stats/intersect2.m
+++ +matspace/+stats/intersect2.m
@@ -2,66 +2,66 @@ function [c, ia, ib] = intersect2(A, B, precision, flag, setOrder) %#codegen
 
 % INTERSECT2  is the same as the built-in intersect, but with a specified precision.
 %
-% SUMMARY:
+% Summary:
 %     This function quantizes the A & B matrices to the specified precision, and then does a normal
 %     intersect command.  Specifying precision = 0 will give the same result as a normal intersect command.
 %
-% INPUT:
+% Input:
 %     A         : (1xA) input vector 1 [num]
 %     B         : (1xB) input vector 2 [num]
 %     precision : (scalar) numerical precision tolerance [num]
 %     flag      : (row) optional flag for operating on rows of matrices [char]
 %     setOrder  : (row) optional string for setting the order of outputs [char] - see note 1
 %
-% OUTPUT:
+% Output:
 %                       where N = number of unique elements in both A & B
 %     c         : (1xN) values in both A & B as quantized by the precision input [num]
 %     ia        : (1xN) index for A to build c [num]
 %     ib        : (1xN) index for B to build c [num]
 %
-% PROTOTYPE:
-%     A         = [1 2 3 3 5 6];
-%     B         = [3.0000000001 4.9999999999];
-%     precision = 1e-6;
-%     [c,ia,ib] = intersect2(A,B,precision)
+% Prototype:
+%     A           = [1 2 3 3 5 6];
+%     B           = [3.0000000001 4.9999999999];
+%     precision   = 1e-6;
+%     [c, ia, ib] = matspace.stats.intersect2(A, B, precision);
 %     % case 2: this case causes problems with the original implementation
-%     A         = [1.000001 1.99999975];
-%     B         = [1.0000015 2.00000025];
-%     precision = 1e-6;
-%     [c,ia,ib] = intersect2(A,B,precision)
+%     A           = [1.000001 1.99999975];
+%     B           = [1.0000015 2.00000025];
+%     precision   = 1e-6;
+%     [c, ia, ib] = matspace.stats.intersect2(A, B, precision);
 %
-% SEE ALSO:
+% See Also:
 %     intersect
 %
-% REFERENCE:
-%     (NONE)
-%
-% NOTES:
+% Notes:
 %     1.  Intersect was updated in MATLAB 2012A and includes additional inputs that were not part
 %         of earlier releases.  This function will support either version
 %
-% CHANGE LOG:
-%     1.  Written by David Stauffer in Feb 2010.
-%     2.  Updated by David Stauffer in Mar 2010 for PGPR 1045.
-%     3.  Updated by David Stauffer in Jul 2012 to make more robust to quantization problems.
-%
-% EXPORT CONTROL WARNING:
-%     (NONE)
-%
-% CLASSIFICATION:
-%     (NONE)
+% Change Log:
+%     1.  Written by David C. Stauffer in Feb 2010.
+%     2.  Updated by David C. Stauffer in Mar 2010 for PGPR 1045.
+%     3.  Updated by David C. Stauffer in Jul 2012 to make more robust to quantization problems.
+%     4.  Updated by David C. Stauffer in June 2020 to finally work on all boundary cases.
 
-% determine the number of optional arguments that were passed in
+% define the appropriate function based on the input arguments
+is_stable = false;
 switch nargin
     case 2
         precision = 0;
-        opt_flags = 0;
+        func = @(a, b) intersect(a, b);
     case 3
-        opt_flags = 0;
+        func = @(a, b) intersect(a, b);
     case 4
-        opt_flags = 1;
+        func = @(a, b) intersect(a, b, flag);
+        if strcmpi(flag, 'stable')
+            is_stable = true;
+        end
     case 5
-        opt_flags = 2;
+        % this version will have a warning on old versions of MATLAB prior to 2012A
+        func = @(a, b) intersect(a, b, flag, setOrder);
+        if strcmpi(flag, 'stable') || strcmpi(setOrder, 'stable')
+            is_stable = true;
+        end
     otherwise
         error('matspace:UnexpectedNargin', 'Unexpected number of inputs: "%i"', nargin);
 end
@@ -69,59 +69,62 @@ end
 % allow a zero precision to be passed in and behave like the normal intersect command
 if precision == 0
     % do a normal intersect
-    switch opt_flags
-        case 0
-            [c, ia, ib] = intersect(A, B);
-        case 1
-            [c, ia, ib] = intersect(A, B, flag);
-        case 2
-            % this line and others like it will have a warning on old versions of MATLAB prior to 2012A
-            [c, ia, ib] = intersect(A, B, flag, setOrder);
-    end
+    [c, ia, ib] = func(A, B);
     return
 end
 
+% determine if all the inputs and integers
+all_int = isinteger(A) && isinteger(B) && isinteger(precision);
 % find larger max of A or B, must call max three times to support up to 3D matrices A and B
 maxAorB = max(max(max(abs(A))), max(max(max(abs(B)))));
 % check if largest component of A and B is too close to the precision floor
-if (maxAorB/precision) > (0.01/eps)
+if ~all_int && ((maxAorB/precision) > (0.01/eps))
     warning('matspace:intersectPrecision','This function may have problems if precision gets too small');
-end
-
-% create a quantized version of A & B
-A2 = precision*round(A/precision);
-B2 = precision*round(B/precision);
-% do a normal intersect on the quantized data
-switch opt_flags
-    case 0
-        [~, ia1, ib1] = intersect(A2, B2);
-    case 1
-        [~, ia1, ib1] = intersect(A2, B2, flag);
-    case 2
-        [~, ia1, ib1] = intersect(A2, B2, flag, setOrder);
 end
 
 % due to the splitting of the quanta, two very close numbers could still fail the intersect.
 % fix this by repeating the comparsion when shifted by half a quanta.
-A2 = precision*round((A+precision/2)/precision) - precision/2;
-B2 = precision*round((B+precision/2)/precision) - precision/2;
-% do a normal intersect on the shifted and quantized data
-switch opt_flags
-    case 0
-        [~, ia2, ib2] = intersect(A2, B2);
-    case 1
-        [~, ia2, ib2] = intersect(A2, B2, flag);
-    case 2
-        [~, ia2, ib2] = intersect(A2, B2, flag, setOrder);
+half_precision = double(precision) / 2;
+if all_int
+    lo_tol = cast(floor(half_precision), class(precision));
+    hi_tol = cast(ceil(half_precision), class(precision));
+else
+    lo_tol = half_precision;
+    hi_tol = half_precision;
 end
 
+% create a quantized version of A & B, plus each one shifted by half a quanta in either direction
+A1 = floor(A / precision);
+B1 = floor(B / precision);
+A2 = floor((A - lo_tol) / precision);
+B2 = floor((B - lo_tol) / precision);
+A3 = floor((A + hi_tol) / precision);
+B3 = floor((B + hi_tol) / precision);
+
+% do a normal intersect on the quantized data
+[~, ia1, ib1] = func(A1, B1);
+[~, ia2, ib2] = func(A1, B2);
+[~, ia3, ib3] = func(A1, B3);
+[~, ia4, ib4] = func(A2, B1);
+[~, ia5, ib5] = func(A3, B1);
+
 % combine the results
-ia = union(ia1, ia2);
-ib = union(ib1, ib2);
+ia = union(union(union(union(ia1, ia2), ia3), ia4), ia5);
+ib = union(union(union(union(ib1, ib2), ib3), ib4), ib5);
 
 % calculate output
-if opt_flags >= 1 && strcmpi(flag,'rows')
+if nargin >= 4 && strcmpi(flag,'rows')
     c = A(ia,:);
-else
-    c = A(ia);
+    return
+end
+
+% since you don't know which of the five intersects will find each index, then you need to make them
+% 'sorted' or 'stable' after the fact.  They will come sorted by index, which gives stable order
+% in the output c
+c = A(ia);
+if ~is_stable && ~issorted(c)
+    [c, ix1] = sort(c);
+    ia = ia(ix1);
+    [~, ix2] = sort(B(ib));
+    ib = ib(ix2);
 end
--- tests/test_intersect2.m
+++ tests/test_intersect2.m
@@ -1,7 +1,19 @@
 classdef test_intersect2 < matlab.unittest.TestCase %#ok<*PROP>
 
 % Tests the fix_rollover function with the following cases:
-%     TBD
+%     Nominal
+%     Floats (x3)
+%     Unique
+%     No index outputs
+%     Tolerance
+%     Tolerance without index outputs
+%     Integers as double and as ints
+%     Ints with Even tolerance
+%     Ints with Odd tolerance
+%     Large ints with tolerances (x3)
+%     Rows (x2)
+%     Rows & Order
+%     Stable
 
     methods (Test)
         function test_nominal(self)
@@ -22,6 +34,26 @@ classdef test_intersect2 < matlab.unittest.TestCase %#ok<*PROP>
             self.verifyEqual(ib, [3; 4; 5])
         end
 
+        function test_floats2(self)
+            a           = [1 2 3 3 5 6];
+            b           = [3.0000000001 4.9999999999];
+            precision   = 1e-6;
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
+            self.verifyEqual(c, [3, 5]);
+            self.verifyEqual(ia, [3; 5]);
+            self.verifyEqual(ib, [1; 2]);
+        end
+
+        function test_floats3(self)
+            a           = [1.000001; 1.99999975];
+            b           = [1.0000015; 2.00000025];
+            precision   = 1e-6;
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
+            self.verifyEqual(c, a);
+            self.verifyEqual(ia, [1; 2]);
+            self.verifyEqual(ib, [1; 2]);
+        end
+
         function test_unique(self)
             a = [1 2.5 4 6];
             b = [0 8 2.5 4 6];
@@ -29,7 +61,8 @@ classdef test_intersect2 < matlab.unittest.TestCase %#ok<*PROP>
             self.verifyEqual(c, [2.5 4 6]);
             self.verifyEqual(ia, [2; 3; 4]);
             self.verifyEqual(ib, [3; 4; 5]);
-            [c, ia, ib] = matspace.stats.intersect2(a, b, 1e-7);
+            precision = 1e-7;
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
             self.verifyEqual(c, [2.5 4 6]);
             self.verifyEqual(ia, [2; 3; 4]);
             self.verifyEqual(ib, [3; 4; 5]);
@@ -45,7 +78,8 @@ classdef test_intersect2 < matlab.unittest.TestCase %#ok<*PROP>
         function test_tolerance(self)
             a = [1 2 3.1 3.9 4 6];
             b = [2 3 4 5];
-            [c, ia, ib] = matspace.stats.intersect2(a, b, 0.12);
+            precision = 0.12;
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
             self.verifyEqual(c, [2 3.1 3.9 4]);
             self.verifyEqual(ia, [2; 3; 4; 5]);
             self.verifyEqual(ib, [1; 2; 3]);
@@ -54,79 +88,131 @@ classdef test_intersect2 < matlab.unittest.TestCase %#ok<*PROP>
         function test_tolerance_no_ix(self)
             a = [1 3 5 7 9];
             b = [1.01 2.02 3.03 4.04 5.05 6.06 7.07 8.08 9.09];
-            c = matspace.stats.intersect2(a, b, 0.055);
+            precision = 0.055;
+            c = matspace.stats.intersect2(a, b, precision);
             self.verifyEqual(c, [1, 3, 5]);
-            c2 = matspace.stats.intersect2(b, a, 0.055);
+            c2 = matspace.stats.intersect2(b, a, precision);
             self.verifyEqual(c2, [1.01 3.03 5.05]);
         end
 
         function test_int(self)
-            % TODO: force this to use ints and non-ints
+            % first test as doubles that are ints
             a = [0 4 10 20 30 -40 30];
             b = [1 5 7 31 -10 -40];
-            [c, ia, ib] = matspace.stats.intersect2(a, b, 0);
+            precision = 0;
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
             self.verifyEqual(c, -40);
             self.verifyEqual(ia, 6);
             self.verifyEqual(ib, 6);
+            % then test with actual int types
             [c, ia, ib] = matspace.stats.intersect2(int32(a), int32(b), int32(0));
-            self.verifyEqual(c, -40);
+            self.verifyEqual(c, int32(-40));
             self.verifyEqual(ia, 6);
             self.verifyEqual(ib, 6);
         end
 
         function test_int_even_tol(self)
-            % TODO: force this to use ints
-            a = [0 4 10 20 30 -40 30];
-            b = [1 5 7 31 -10 -40];
-            [c, ia, ib] = matspace.stats.intersect2(a, b, 2);
-            self.verifyEqual(c, [-40 0 4 30]);
-            self.verifyEqual(ia, [1; 2; 5; 6]);
-            self.verifyEqual(ib, [1; 2; 4; 6]);
+            a = int32([0 4 10 20 30 -40 30]);
+            b = int32([1 5 7 31 -10 -40]);
+            precision = int32(2);
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
+            self.verifyEqual(c, int32([-40 0 4 30]));
+            self.verifyEqual(ia, [6; 1; 2; 5]);
+            self.verifyEqual(ib, [6; 1; 2; 4]);
         end
 
         function test_int_odd_tol(self)
-            % TODO: force this to use ints
-            a = [0 4 10 20 30 -40 30];
-            b = [1 5 7 31 -10 -40];
-            [c, ia, ib] = matspace.stats.intersect2(a, b, 3);
-            self.verifyEqual(c, [-40 0 4 10 30]);
-            self.verifyEqual(ia, [1; 2; 3; 5; 6]);
-            self.verifyEqual(ib, [1; 2; 3; 4; 6]);
+            a = int32([0 4 10 20 30 -40 30]);
+            b = int32([1 5 7 31 -10 -40]);
+            precision = int32(3);
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
+            self.verifyEqual(c, int32([-40 0 4 10 30]));
+            self.verifyEqual(ia, [6; 1; 2; 3; 5]);
+            self.verifyEqual(ib, [6; 1; 2; 3; 4]);
         end
-        
+
         function test_int64(self)
             % TODO: force this to use ints
-            t_offset = 2^62;
-            a = [0 4 10 20 30 -40 30] + t_offset;
-            b = [1 5 7 31 -10 -40] + t_offset;
-            [c, ia, ib] = matspace.stats.intersect2(a, b, 0);
-            self.verifyEqual(c, -40 + t_offset);
-            self.verifyEqual(ia, 5);
-            self.verifyEqual(ib, 5);
+            t_offset = int64(2)^62;
+            a = int64([0 4 10 20 30 -40 30]) + t_offset;
+            b = int64([1 5 7 31 -10 -40]) + t_offset;
+            precision = int64(0);
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
+            self.verifyEqual(c, int64(-40) + t_offset);
+            self.verifyEqual(ia, 6);
+            self.verifyEqual(ib, 6);
         end
-        
+
         function test_int64_even_tol(self)
-            % TODO: force this to use ints
-            t_offset = 2^62;
-            a = [0 4 10 20 30 -40 30] + t_offset;
-            b = [1 5 7 31 -10 -40] + t_offset;
-            [c, ia, ib] = matspace.stats.intersect2(a, b, 2);
-            self.verifyEqual(c, [-40 0 4 30] + t_offset);
-            self.verifyEqual(ia, [1; 2; 5; 6]);
-            self.verifyEqual(ib, [1; 2; 4; 6]);
+            t_offset = int64(2)^62;
+            a = int64([0 4 10 20 30 -40 30]) + t_offset;
+            b = int64([1 5 7 31 -10 -40]) + t_offset;
+            precision = int64(2);
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
+            self.verifyEqual(c, int64([-40 0 4 30]) + t_offset);
+            self.verifyEqual(ia, [6; 1; 2; 5]);
+            self.verifyEqual(ib, [6; 1; 2; 4]);
         end
 
         function test_int64_odd_tol(self)
-            % TODO: force this to use ints
-            t_offset = 2^62;
-            a = [0 4 10 20 30 -40 30] + t_offset;
-            b = [1 5 7 31 -10 -40] + t_offset;
-            [c, ia, ib] = matspace.stats.intersect2(a, b, 3);
-            self.verifyEqual(ia, [1; 2; 3; 5; 6]);
-            self.verifyEqual(ib, [1; 2; 3; 4; 6]);
-            self.verifyEqual(c, [-40 0 4 10 30] + t_offset);
+            t_offset = int64(2)^62;
+            a = int64([0 4 10 20 30 -40 30]) + t_offset;
+            b = int64([1 5 7 31 -10 -40]) + t_offset;
+            precision = int64(3);
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision);
+            self.verifyEqual(ia, [6; 1; 2; 3; 5]);
+            self.verifyEqual(ib, [6; 1; 2; 3; 4]);
+            self.verifyEqual(c, int64([-40 0 4 10 30]) + t_offset);
         end
-        
-        % TODO: add rows, add stable
+
+        function test_rows(self)
+            a = [1 2 3 4; 5 6 7 8];
+            b = [2 4 6 8; 1 3 5 7];
+            precision = 0.1;
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision, 'rows');
+            self.verifyEqual(c, zeros(0, 4));
+            self.verifyEqual(ia, zeros(0, 1));
+            self.verifyEqual(ib, zeros(0, 1));
+        end
+
+        function test_rows2(self)
+            a = [1 2; 3 4; 5 6; 7 8; 9.01 9.999];
+            b = [1 3; 3 4.01; 6 5; 9 10; 7 7];
+            precision = 0.1;
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision, 'rows');
+            self.verifyEqual(c, [3 4; 9.01 9.999]);
+            self.verifyEqual(ia, [2; 5]);
+            self.verifyEqual(ib, [2; 4]);
+        end
+
+        function test_rows_and_order(self)
+            a = [10 10; 1 2; 5 6; 3 4; 8 8];
+            b = [1 2.1; 3 4; 5.1 6.1; 8 9];
+            precision = 0.2;
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision, 'rows', 'sorted');
+            self.verifyEqual(c, [1 2; 5 6; 3 4]); % Note: currently this option doesn't actually do the sort as sorting a row isn't really defined
+            self.verifyEqual(ia, [2; 3; 4]);
+            self.verifyEqual(ib, [1; 2; 3]);
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision, 'rows', 'stable');
+            self.verifyEqual(c, [1 2; 5 6; 3 4]);
+            self.verifyEqual(ia, [2; 3; 4]);
+            self.verifyEqual(ib, [1; 2; 3]);
+        end
+
+        function test_stable(self)
+            a = [10 20 -30 40 -50 65 70];
+            b = (-100:10:100) + 0.01 * rand([1 21]);
+            precision = 0.01;
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision, 'sorted');
+            self.verifyEqual(c, [-50 -30 10 20 40 70]);
+            self.verifyEqual(ia, [5; 3; 1; 2; 4; 7]);
+            self.verifyEqual(ib, [6; 8; 12; 13; 15; 18]);
+            [c, ia, ib] = matspace.stats.intersect2(a, b, precision, 'stable');
+            self.verifyEqual(c, [10 20 -30 40 -50 70]);
+            self.verifyEqual(ia, [1; 2; 3; 4; 5; 7]);
+            self.verifyEqual(ib, [6; 8; 12; 13; 15; 18]);
+        end
+
+        % TODO: add matrices
     end
 end
