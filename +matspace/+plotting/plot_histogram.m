7509c8516d6c062ebdc43610f8adde0a7c4de357
David Stauffer <dstauffman@yahoo.com>
2021-01-19 09:55:22 -0800

    Initial release of histogram plot.

---
+++ +matspace/+plotting/plot_histogram.m
@@ -0,0 +1,56 @@
+function [fig_hand] = plot_histogram(data, edges, OPTS, varargin)
+
+% PLOT_HISTOGRAM  plots the histogram of the given data with the given edges
+%
+% Prototype:
+%     data = randn(10000, 1);
+%     edges = [-3 -2 -1 -0.5 0 0.25, 0.5 0.75, 1.0, 1.5, 2.0, 3.0, 5.0];
+%     OPTS = matspace.plotting.Opts();
+%     matspace.plotting.plot_histogram(data, edges, OPTS, 'Description', 'Example Histogram');
+
+%% Imports
+import matspace.plotting.figmenu
+import matspace.plotting.plot_second_yunits
+import matspace.plotting.setup_plots
+
+%% Parser
+% Argument parser
+p = inputParser;
+addParameter(p, 'Description', '', @ischar);
+parse(p, varargin{:});
+description = p.Results.Description;
+
+%% Calculations
+% histogram
+N = histcounts(data, edges);
+widths = edges(2:end) - edges(1:end-1);
+mid_pts = edges(1:end-1) + 0.5 * widths;
+if OPTS.show_plot
+    fig_visible = 'on';
+else
+    fig_visible = 'off';
+end
+
+%% Plot
+fig_hand = figure('Name', description, 'Visible', fig_visible);
+ax = axes(fig_hand);
+hold(ax, 'on');
+% bar(ax, mid_pts, N);
+for i = 1:length(N)
+    patch(ax, 'Faces', [1 2 3 4], 'Vertices', [edges(i) 0; edges(i+1) 0; edges(i+1) N(i); edges(i) N(i)], ...
+        'FaceColor', [0.000 0.447 0.741], 'EdgeColor', 'k');
+end
+title(ax, description, 'Interpreter', 'None');
+xlabel(ax, 'Bins');
+ylabel(ax, 'Number');
+plot_second_yunits(ax, 'Percentage', 100 / sum(N));
+hold(ax, 'off');
+
+%% Format plots
+setup_plots(fig_hand, OPTS, 'dist_no_y_scale');
+if OPTS.show_plot
+    figmenu;
+else
+    close(fig_hand);
+    fig_hand = [];
+end
